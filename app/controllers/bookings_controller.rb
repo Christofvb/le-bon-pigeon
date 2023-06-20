710775ee7df0a7512a0a692163d8990b9ca21740
Christofvb <christof.vanberendonck@hotmail.com>
2023-06-20 15:49:14 +0200

    homepage

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,6 @@
 class BookingsController < ApplicationController
     before_action :set_bookmark, only: :destroy
-    before_action :set_pigeon, only:  [:create, :show]
+    before_action :set_pigeon, only: [:create, :show]
 
     def index
       @bookings = Booking.all
@@ -16,7 +16,7 @@ class BookingsController < ApplicationController
           redirect_to bookings_path(@pigeon)
         else
           render :new
-        end    
+        end
     end
 
     def destroy
@@ -29,12 +29,12 @@ class BookingsController < ApplicationController
     def booking_params
       params.require(:booking).permit(:date_start, :date_end, :user_id)
     end
-  
+
     # pour destroy
     # def set_booking
     #   @booking = Booking.find(params[:id])
     # end
-  
+
     def set_pigeon
       @pigeon = Pigeon.find(params[:pigeon_id])
     end
