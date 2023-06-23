290076bc4c1ad4c60f27f355cd01bff034b8c127
MrBabon <christophedanna06@gmail.com>
2023-06-23 15:43:43 +0200

    la main dans le sac

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -23,7 +23,6 @@ class BookingsController < ApplicationController
   def destroy
     @booking.destroy
 
-    redirect_to bookings_path, status: :see_other
 
     flash[:notice] = "La réservation a été supprimée avec succès."
     redirect_to bookings_path
