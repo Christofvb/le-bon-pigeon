f490cb67a3dc18c597d2b9dc8dfee885d1e8de1e
Christofvb <christof.vanberendonck@hotmail.com>
2023-06-23 14:52:41 +0200

    son et booking

--- app/controllers/pigeons_controller.rb
+++ app/controllers/pigeons_controller.rb
@@ -1,17 +1,16 @@
 class PigeonsController < ApplicationController
-    def index
-      if params[:query]
-        @pigeons = Pigeon.search_by_pigeon(params[:query])
-      else
-        @pigeons = Pigeon.all
-      end
-      params[:query] = ""
+  def index
+    if params[:query]
+      @pigeons = Pigeon.search_by_pigeon(params[:query])
+    else
+      @pigeons = Pigeon.all
     end
+    params[:query] = ""
+  end
 
-    def show
-      @pigeon = Pigeon.find(params[:id])
-      @booking = Booking.new
-      @bookings = Booking.all
-    end
-
+  def show
+    @pigeon = Pigeon.find(params[:id])
+    @booking = Booking.new
+    @bookings = Booking.all
+  end
 end
